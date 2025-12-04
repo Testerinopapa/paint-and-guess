904e51fd72cea806992711bf9cf4fea4ead79335
TesterinoPapa <latin007protocol171@aol.com>
2025-12-04 16:45:35 -0300

    Addedsvgpieces8

--- src/games/chess/utils/chessPieces.tsx
+++ src/games/chess/utils/chessPieces.tsx
@@ -79,6 +79,7 @@ export const WhiteKnight = ({ size = 60, className }: PieceProps) => {
       <path d="M24 18c.38 2.91-5.55 7.37-8 9-3 2-2.82 4.34-5 4-1.04-.74 1.73-2.9 2-3 .5-2 4-3.5 6-3.5 2.5 0 4.5 1.5 5 3.5z" fill="#fff" stroke="#000" strokeLinecap="butt" strokeWidth="1.5"/>
       <path d="M9.5 25.5a.5.5 0 1 1-1 0 .5.5 0 1 1 1 0z" fill="#000" stroke="#000" strokeWidth="0.5"/>
       <path d="M14.933 15.75a.5 1.5 30 1 1-.866-.5.5 1.5 30 1 1 .866.5z" fill="#000" stroke="#000" strokeWidth="1.5"/>
+      <path d="M24.55 10.4l-.45 1.45.5.15c3.15 1 5.65 2.49 7.9 6.75S35.75 29.06 35.25 39l-.05.5h2.25l.05-.5c.5-10.06-.88-16.85-3.25-21.34-2.37-4.49-5.79-6.64-9.19-7.16l-.51-.1z" fill="#fff" stroke="none"/>
     </g>
     <rect x="0" y="0" width="45" height="45" fill="none" stroke="red" strokeWidth="0.5" strokeDasharray="2,2" opacity="0.3"/>
   </svg>
@@ -162,6 +163,7 @@ export const BlackKnight = ({ size = 60, className }: PieceProps) => {
       <path d="M24 18c.38 2.91-5.55 7.37-8 9-3 2-2.82 4.34-5 4-1.04-.74 1.73-2.9 2-3 .5-2 4-3.5 6-3.5 2.5 0 4.5 1.5 5 3.5z" fill="#000" stroke="#000" strokeLinecap="butt" strokeWidth="1.5"/>
       <path d="M9.5 25.5a.5.5 0 1 1-1 0 .5.5 0 1 1 1 0z" fill="#fff" stroke="#fff" strokeWidth="0.5"/>
       <path d="M14.933 15.75a.5 1.5 30 1 1-.866-.5.5 1.5 30 1 1 .866.5z" fill="#fff" stroke="#fff" strokeWidth="1.5"/>
+      <path d="M24.55 10.4l-.45 1.45.5.15c3.15 1 5.65 2.49 7.9 6.75S35.75 29.06 35.25 39l-.05.5h2.25l.05-.5c.5-10.06-.88-16.85-3.25-21.34-2.37-4.49-5.79-6.64-9.19-7.16l-.51-.1z" fill="#000" stroke="none"/>
     </g>
     <rect x="0" y="0" width="45" height="45" fill="none" stroke="red" strokeWidth="0.5" strokeDasharray="2,2" opacity="0.3"/>
   </svg>
